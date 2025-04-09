6ccff9235b269c106915e278ce7ae7ad74d0b9f4
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2025-04-09 11:45:32 -0400

    added onclick handler

--- src/List.jsx
+++ src/List.jsx
@@ -1,15 +1,31 @@
 
+import { useState } from 'react';
+import './assets/List.css'
 
-const { Fragment } = React;
+const List = ({ items }) => {
+  const [selected, setSelected] = useState([]);
 
-const List = ({ items }) => (
-  <Fragment>
+  const handleSelect = (ev) => {
+    console.log('ev', ev)
+  }
+
+  return (
+    <>
     <ul className="List">
       {items.map(item => (
-        <li key={item.name} className={`List__item List__item--${item.color}`}>
+          <li
+            key={item.name}
+            className={`
+              List__item List__item--${item.color}
+            `}
+            onClick={handleSelect}
+          >
           {item.name}
         </li>
       ))}
     </ul>
-  </Fragment>
+    </>
 );
+}
+
+export default List;
